53491a838df4bb29a8817bfe26fa5153a3c670ae
sstack124 <Tara658865>
2022-08-26 16:31:44 -0400

    Initial Application

--- application.py
+++ application.py
@@ -6,9 +6,3 @@ application = Flask(__name__)
 @application.route('/')
 def hello_world():
     return 'Hello World'
-
-
-@application.route('/user', methods=['GET'])
-def view_user():
-    if request.method == 'GET':
-        return "Hello, this is a test page."
