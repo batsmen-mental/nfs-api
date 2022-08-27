ab5456b560ea5db37787d9cb701bfb582f7b700f
sstack124 <Tara658865>
2022-08-27 17:42:37 -0400

    Initial Application

--- application.py
+++ application.py
@@ -4,3 +4,7 @@ application = Flask(__name__)
 @application.route('/', methods=['GET'])
 def index():
     return'Hello World'
+
+@application.route('/user', methods=['GET'])
+def user():
+    return '{"userid":"123","username":"testuser","first_name":"Jim","last_name":"Smith"}'
