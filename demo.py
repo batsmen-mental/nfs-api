aed1042baf1f4c4ef4a1ca27f9f151d05e5bc590
sstack124 <sstack124@verizon.net>
2023-09-22 15:05:29 -0400

    Debugging Errors

--- demo.py
+++ demo.py
@@ -4,13 +4,13 @@ demo = Blueprint("demo", __name__)
 
 @demo.route('/', methods=['POST', 'GET', 'DELETE'])
 def main():
-
+    inspector_id = 1
     Host = "nfi-demo-database.c2cn8z8gktfp.us-east-1.rds.amazonaws.com"
     Port = 3306
     User = "admin"
     Password = "nfiadminpwd1!"
     database = "demodb"
-    query = "SELECT * FROM company"
+    query = f"select i.inspection_id, i.start_date, c.name  from inspections i, customer c  where i.customer_id = c.customer_id  AND inspector_id = {int(inspector_id)} and DATE_ADD(start_date , INTERVAL(-WEEKDAY(start_date)) DAY) = DATE_ADD(CURDATE() , INTERVAL(-WEEKDAY(CURDATE())) DAY);"
     mydb = mysql.connector.connect(host=Host, user=User, port=Port, password=Password, database=database,
                                    charset='utf8')
     mycursor = mydb.cursor(buffered=True, dictionary=True)
