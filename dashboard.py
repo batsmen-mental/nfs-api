7dbaeb6bc1beb51c705b63dda0061108cece4157
sstack124 <sstack124@verizon.net>
2023-09-22 15:38:54 -0400

    Debugging Errors

--- dashboard.py
+++ dashboard.py
@@ -35,6 +35,8 @@ def query_db(query):
     User = "admin"
     Password = "nfiadminpwd1!"
     database = "demodb"
+    inspector_id = 1
+    query = f"SELECT i.inspection_id, i.start_date, c.name  from inspections i, customer c  where i.customer_id = c.customer_id  AND inspector_id = {int(inspector_id)} and DATE_ADD(start_date , INTERVAL(-WEEKDAY(start_date)) DAY) = DATE_ADD(CURDATE() , INTERVAL(-WEEKDAY(CURDATE())) DAY);"
     mydb = mysql.connector.connect(host=Host, user=User, port=Port, password=Password, database=database,
                                    charset='utf8')
     mycursor = mydb.cursor(buffered=True, dictionary=True)
@@ -42,9 +44,10 @@ def query_db(query):
     if "SELECT" in query:
         results = mycursor.fetchall()
         mydb.commit()
+
         # return (str(results).replace('[','').replace('(','').replace(']','').replace('\'','').replace(' ','').replace(')',''), 500)
-        return (results, 200)
+        return (results, 500)
     else:
         mydb.commit()
-        return ('Successful', 200)
+        return ('Successful', 500)
     mydb.close()
