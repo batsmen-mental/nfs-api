abaaf499d5669627a3c4a400ee6372403f695778
sstack124 <sstack124@verizon.net>
2023-09-22 14:48:54 -0400

    Debugging Errors

--- application.py
+++ application.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from users import users
 from dashboard import dashboard
-from test import test
 application = Flask(__name__)
 
 application.register_blueprint(users, url_prefix="/users")
 application.register_blueprint(dashboard, url_prefix="/dashboard")
-application.register_blueprint(test, url_prefix="/test")
 
 @application.route('/', methods=['GET'])
 def index():
