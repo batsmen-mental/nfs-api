9fe53b7c97ce3e3788214b8e7e89f4947c8ddb1a
sstack124 <sstack124@verizon.net>
2023-09-22 16:15:34 -0400

    Debugging Errors

--- dashboard.py
+++ dashboard.py
@@ -12,12 +12,13 @@ def main_dashboard():
         try:
             userid = request.args.get('userid', None)
             demodata = request.args.get('demodata', None)
+            starting_week = request.args.get('starting_week', start)
             try:
                 if demodata:
                     response = '{"WeekInfo":[{"Week_Starting":"09/11/2023","JobList":[{"JobID":1911,"JobDate":"09/11/2023","Customer":"Crazy Nails"},{"JobID":1923,"JobDate":"09/12/2023","Customer":"Top Fashion"},{"JobID":1924,"JobDate":"09/13/2023","Customer":"LazerTag Empire"},{"JobID":1931,"JobDate":"09/15/2023","Customer":"Yummy Stuff"}]},{"Week_Starting":"09/18/2023","JobList":[{"JobID":1964,"JobDate":"09/18/2023","Customer":"Best Hospital"},{"JobID":1999,"JobDate":"09/21/2023","Customer":"On Fire BBQ"},{"JobID":1944,"JobDate":"09/22/2023","Customer":"Water Source LLC"}]}]}'
                     return (response, 200)
                 elif userid is not None:
-                    query = f"SELECT i.inspection_id, i.start_date, c.name  from inspections i, customer c  where i.customer_id = c.customer_id  AND inspector_id = {userid} and starting_week = '{start}';"
+                    query = f"SELECT i.inspection_id, i.start_date, c.name  from inspections i, customer c  where i.customer_id = c.customer_id  AND inspector_id = {userid} and starting_week = '{starting_week}';"
                     response = query_db(query)
                     return (response, 200)
                 else:
