a29363209cf286db577e74430976c0d3079ca97d
sstack124 <sstack124@verizon.net>
2023-09-22 15:07:08 -0400

    Debugging Errors

--- demo.py
+++ demo.py
@@ -10,7 +10,7 @@ def main():
     User = "admin"
     Password = "nfiadminpwd1!"
     database = "demodb"
-    query = f"select i.inspection_id, i.start_date, c.name  from inspections i, customer c  where i.customer_id = c.customer_id  AND inspector_id = {int(inspector_id)} and DATE_ADD(start_date , INTERVAL(-WEEKDAY(start_date)) DAY) = DATE_ADD(CURDATE() , INTERVAL(-WEEKDAY(CURDATE())) DAY);"
+    query = f"SELECT i.inspection_id, i.start_date, c.name  from inspections i, customer c  where i.customer_id = c.customer_id  AND inspector_id = {int(inspector_id)} and DATE_ADD(start_date , INTERVAL(-WEEKDAY(start_date)) DAY) = DATE_ADD(CURDATE() , INTERVAL(-WEEKDAY(CURDATE())) DAY);"
     mydb = mysql.connector.connect(host=Host, user=User, port=Port, password=Password, database=database,
                                    charset='utf8')
     mycursor = mydb.cursor(buffered=True, dictionary=True)
