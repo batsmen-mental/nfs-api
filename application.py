df6ffee3199c5b1fbb1b7945ea7498cc777c1109
sstack124 <sstack124@verizon.net>
2023-09-22 14:28:49 -0400

    Adding Test DB file

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from users import users
+from test import test
 from dashboard import dashboard
 application = Flask(__name__)
 
 application.register_blueprint(users, url_prefix="/users")
 application.register_blueprint(dashboard, url_prefix="/dashboard")
+application.register_blueprint(test, url_prefix="/test")
 
 @application.route('/', methods=['GET'])
 def index():
--- test.py
+++ test.py
@@ -3,14 +3,15 @@ test = Blueprint("test", __name__)
 
 @dashboard.route('/', methods=['POST', 'GET', 'DELETE'])
 
-def test_db(query):
+def test_db():
     import mysql.connector
 
     Host = "nfi-demo-database.c2cn8z8gktfp.us-east-1.rds.amazonaws.com"
     Port = 3306
     User = "admin"
     Password = "nfiadminpwd1!"
-    database = "test"
+    database = "demodb"
+    query = 'select * from company'
 
     mydb = mysql.connector.connect(host=Host, user=User, port=Port, password=Password, database=database, charset='utf8')
     mycursor = mydb.cursor(buffered=True)
@@ -18,4 +19,5 @@ def test_db(query):
     results = mycursor.fetchall()
     mydb.commit()
     mydb.close()
-    return (str(results).replace('[','').replace('(','').replace(']','').replace('\'','').replace(' ','').replace(')',''),200)
+    # return (str(results).replace('[','').replace('(','').replace(']','').replace('\'','').replace(' ','').replace(')',''),200)
+    return (str(results),200)
