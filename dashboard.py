6798880a158299f1d1092522014e46d5edee9d80
sstack124 <sstack124@verizon.net>
2023-09-22 15:17:59 -0400

    Debugging Errors

--- dashboard.py
+++ dashboard.py
@@ -6,9 +6,8 @@ def main_dashboard():
     valid_username_list = ['inspector','company','owner','firemarshall']
     if request.method == "GET":
         try:
-            #userid = request.args['userid']
-            userid = 1
-            demodata = request.args['demodata']
+            userid = request.args.get('userid', None)
+            demodata = request.args.get('demodata', None)
             try:
                 if demodata:
                     response = '{"WeekInfo":[{"Week_Starting":"09/11/2023","JobList":[{"JobID":1911,"JobDate":"09/11/2023","Customer":"Crazy Nails"},{"JobID":1923,"JobDate":"09/12/2023","Customer":"Top Fashion"},{"JobID":1924,"JobDate":"09/13/2023","Customer":"LazerTag Empire"},{"JobID":1931,"JobDate":"09/15/2023","Customer":"Yummy Stuff"}]},{"Week_Starting":"09/18/2023","JobList":[{"JobID":1964,"JobDate":"09/18/2023","Customer":"Best Hospital"},{"JobID":1999,"JobDate":"09/21/2023","Customer":"On Fire BBQ"},{"JobID":1944,"JobDate":"09/22/2023","Customer":"Water Source LLC"}]}]}'
