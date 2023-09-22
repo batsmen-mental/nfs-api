5416ac246932d00ff6ce5ff06025a1c69c9e9a7b
sstack124 <sstack124@verizon.net>
2023-09-22 14:37:07 -0400

    Updating Requirements

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
 from flask import Blueprint, request
+import mysql.connector
 test = Blueprint("test", __name__)
 
 @dashboard.route('/', methods=['POST', 'GET', 'DELETE'])
-
 def test_db():
-    import mysql.connector
-
     Host = "nfi-demo-database.c2cn8z8gktfp.us-east-1.rds.amazonaws.com"
     Port = 3306
     User = "admin"
     Password = "nfiadminpwd1!"
     database = "demodb"
-    query = 'select * from company'
+    query = "select * from company"
 
     mydb = mysql.connector.connect(host=Host, user=User, port=Port, password=Password, database=database, charset='utf8')
     mycursor = mydb.cursor(buffered=True)
