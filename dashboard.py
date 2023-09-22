83102733f9d0c5d28bd639ac612b21c47776f08d
sstack124 <sstack124@verizon.net>
2023-09-22 13:40:52 -0400

    Fixing Errors

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+import mysql.connector
 dashboard = Blueprint("dashboard", __name__)
 
 @dashboard.route('/', methods=['POST', 'GET', 'DELETE'])
@@ -14,7 +15,7 @@ def main_dashboard():
                     return (response, 200)
                 elif userid is not None:
                     try:
-                        query = f"select i.inspection_id, i.start_date, c.name  from inspections i, customer c  where i.customer_id = c.customer_id  AND inspector_id = {userid} and DATE_ADD(start_date , INTERVAL(-WEEKDAY(start_date)) DAY) = DATE_ADD(CURDATE() , INTERVAL(-WEEKDAY(CURDATE())) DAY);"
+                        query = f"SELECT i.inspection_id, i.start_date, c.name  from inspections i, customer c  where i.customer_id = c.customer_id  AND inspector_id = {userid} and DATE_ADD(start_date , INTERVAL(-WEEKDAY(start_date)) DAY) = DATE_ADD(CURDATE() , INTERVAL(-WEEKDAY(CURDATE())) DAY);"
                         response = run_query(query)
                         # response = '{"WeekInfo":[{"Week_Starting":"09/11/2023","JobList":[{"JobID":1911,"JobDate":"09/11/2023","Customer":"Crazy Nails"},{"JobID":1923,"JobDate":"09/12/2023","Customer":"Top Fashion"},{"JobID":1924,"JobDate":"09/13/2023","Customer":"LazerTag Empire"},{"JobID":1931,"JobDate":"09/15/2023","Customer":"Yummy Stuff"}]},{"Week_Starting":"09/18/2023","JobList":[{"JobID":1964,"JobDate":"09/18/2023","Customer":"Best Hospital"},{"JobID":1999,"JobDate":"09/21/2023","Customer":"On Fire BBQ"},{"JobID":1944,"JobDate":"09/22/2023","Customer":"Water Source LLC"}]}]}'
                         return (response, 200)
@@ -34,8 +35,6 @@ def main_dashboard():
 def run_query(query):
 
     try:
-        import mysql.connector
-
         Host = "nfi-demo-database.c2cn8z8gktfp.us-east-1.rds.amazonaws.com"
         Port = 3306
         User = "admin"
