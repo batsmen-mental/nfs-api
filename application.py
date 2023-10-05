148e253be17bcdd51a1850e4215c2650ecb5de3c
sstack124 <sstack124@verizon.net>
2023-10-05 17:04:42 -0400

    Add Admin Webpage

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from users import users
 from dashboard import dashboard
 from company import company
@@ -13,6 +13,10 @@ application.register_blueprint(customer, url_prefix="/customer")
 @application.route('/', methods=['GET'])
 def index():
     return'Hello World'
+
+@app.route('/admin')
+def hello():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     application.run(debug=True)
