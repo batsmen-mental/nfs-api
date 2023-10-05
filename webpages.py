df64f86605e2b84d3862f9412983a4dda2f644b3
sstack124 <sstack124@verizon.net>
2023-10-05 17:49:53 -0400

    Add Admin Webpage

--- webpages.py
+++ webpages.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, render_template
+from db_actions import query_db
 webpages = Blueprint("webpages", __name__)
 
 @webpages.route('/add_customer', methods=['POST', 'GET', 'DELETE'])
 def main():
-    return render_template('webpages/add_customer.html')
+    query = f"SELECT * FROM customers;"
+    response = query_db(query)
+    return render_template('webpages/add_customer.html', data=response)
