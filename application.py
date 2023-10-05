21155ac014dc235ade38932eed27b47fce9d1080
sstack124 <sstack124@verizon.net>
2023-10-04 20:50:56 -0400

    Adding Company API Call

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from users import users
 from dashboard import dashboard
+from company import company
 application = Flask(__name__)
 
 application.register_blueprint(users, url_prefix="/users")
 application.register_blueprint(dashboard, url_prefix="/dashboard")
+application.register_blueprint(company, url_prefix="/company")
 
 @application.route('/', methods=['GET'])
 def index():
