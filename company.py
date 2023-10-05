aa64bf72168b7d516c0d20949f57d7944c9d0865
sstack124 <sstack124@verizon.net>
2023-10-04 21:11:28 -0400

    Add Company API Call - Updates

--- company.py
+++ company.py
@@ -6,17 +6,21 @@ company = Blueprint("company", __name__)
 def add_company():
     if request.method == "POST":
         try:
-            name = request.form.get('name','')
-            address = request.form.get('address','')
+            name = request.form.get('name',None)
+            address = request.form.get('address',None)
             address2 = request.form.get('address2','')
-            city = request.form.get('city','')
-            state = request.form.get('state','')
-            zip = request.form.get('zip','')
-            county = request.form.get('county','')
-            phone = request.form.get('phone','')
+            city = request.form.get('city',None)
+            state = request.form.get('state',None)
+            zip = request.form.get('zip',None)
+            county = request.form.get('county',None)
+            phone = request.form.get('phone',None)
             phone2 = request.form.get('phone2','')
             fax = request.form.get('fax','')
             website = request.form.get('website','')
+
+            if None in (name, address, city, state, zip, county, phone):
+
+
             try:
                 query = f"INSERT into company (name,address,address2,city,state,zip,county,phone,phone2,fax,website) VALUES('{name}','{address}','{address2}','{city}','{state}','{zip}','{county}','{phone}','{phone2}','{fax}','{website}');"
                 response = query_db(query)
