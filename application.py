2f8eca606440203414cf5dee2d1fccbbb4ed5ca3
sstack124 <sstack124@verizon.net>
2023-10-05 17:29:50 -0400

    Add Admin Webpage

--- application.py
+++ application.py
@@ -10,7 +10,6 @@ application.register_blueprint(users, url_prefix="/users")
 application.register_blueprint(dashboard, url_prefix="/dashboard")
 application.register_blueprint(company, url_prefix="/company")
 application.register_blueprint(customer, url_prefix="/customer")
-application.register_blueprint(customer, url_prefix="/customer")
 application.register_blueprint(webpages, url_prefix="/webpages")
 
 @application.route('/', methods=['GET'])
