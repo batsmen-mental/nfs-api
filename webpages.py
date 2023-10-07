a91606ca10ee712e34fe16cb7c860f94cf917f88
sstack124 <sstack124@verizon.net>
2023-10-06 23:25:15 -0400

    Add Admin Webpage

--- webpages.py
+++ webpages.py
@@ -3,13 +3,13 @@ from db_actions import query_db
 webpages = Blueprint("webpages", __name__)
 
 @webpages.route('/add_customer', methods=['POST', 'GET', 'DELETE'])
-def main():
+def add_customer():
     query = f"SELECT * FROM customer;"
     response = query_db(query)
     return render_template('webpages/add_customer.html', response=response)
 
 @webpages.route('/add_company', methods=['POST', 'GET', 'DELETE'])
-def main():
+def add_company():
     query = f"SELECT * FROM customer;"
     response = query_db(query)
     return render_template('webpages/add_company.html', response=response)
