5e1ca8d2f096e1225241f2eee9e582eb34b63275
sstack124 <sstack124@verizon.net>
2023-10-06 22:33:17 -0400

    Add Admin Webpage

--- webpages.py
+++ webpages.py
@@ -6,8 +6,9 @@ webpages = Blueprint("webpages", __name__)
 def main():
     query = f"SELECT * FROM customer;"
     response = query_db(query)
-    response = [{ "News" : "ABC", "Link" : "http://exampleabc.com", "Date": "01/03/20" },
-           { "News" : "DEF", "Link" : "http://exampledef.com", "Date": "02/03/20" },
-           { "News" : "GHI", "Link" : "http://exampleghi.com", "Date": "03/03/20" }]
+    # response = [{ "News" : "ABC", "Link" : "http://exampleabc.com", "Date": "01/03/20" },
+    #        { "News" : "DEF", "Link" : "http://exampledef.com", "Date": "02/03/20" },
+    #        { "News" : "GHI", "Link" : "http://exampleghi.com", "Date": "03/03/20" }]
     # return (response, 200)
-    return render_template('webpages/add_customer.html', results=response)
+    # return render_template('webpages/add_customer.html', results=response)
+    return render_template('webpages/add_customer.html')
